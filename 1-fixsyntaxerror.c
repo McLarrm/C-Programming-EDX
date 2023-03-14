557abae3aaf1cc7dcce873e4c263ef05d009dfb9
Olajide <larrm_ola@live.com>
2023-03-14 16:24:45 -0700

    Fixing syntax error

---
+++ 1-fixsyntaxerror.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - We already need you! Our full-time developer went on sick leave, leaving us with an erroneous C-program without telling us how to fix it.
+ * All we have are the compiler error messages produced by this code.Please help us fix this code. Here is the program in question
+ * #include <stdio.h>
+ * int main(void)
+ * {
+    printf ("This is code with errors: ")
+    printf "Fix them!";
+    return 0;
+    }
+ * Return: 0
+ */
+ int main(void)
+ {
+     printf("This is code with errors: ");
+     printf("Fix them!");
+     return 0;
+ }
