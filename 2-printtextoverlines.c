d806b7a418711f9507bc81e565130f20a1077974
Olajide <larrm_ola@live.com>
2023-03-14 16:34:26 -0700

    Printing multiple lines

--- 2-printtextoverlines.c
+++ 2-printtextoverlines.c
@@ -1,6 +1,5 @@
 //We just showed you how to print text to the screen and in particular how to display multiple lines of text. In this activity, the goal is to print a list of skills you have already mastered to the screen.
 //The list of skills is the following:
-
 //I already know how to:
 //- Print text to the screen.
 //- Start a new line.
