4b152324076ad592695e1488258d8125350872cc
Olajide <larrm_ola@live.com>
2023-03-14 16:43:59 -0700

    Using for loop

---
+++ 5-userforlooptomultipleprintlines.c
@@ -0,0 +1,23 @@
+//You would like to display the same line of text several times, but you do not wish to have to type it multiple times in your program.
+//Please print the following text to the screen:
+//C is fun!
+//C is fun!
+//C is fun!
+#include <stdio.h>
+
+/**
+ * main - Printing a word using for loop
+ * 
+ * Return: 0
+ */
+ int main(void)
+ {
+     int i;
+     {
+         for (i = 0; i < 3; i++)
+         printf("C is fun!\n");
+     }
+     
+     return (0);
+ }
+
