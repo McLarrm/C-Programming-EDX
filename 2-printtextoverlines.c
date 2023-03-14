2819f242301f4a73669a6d15640f2e072aae203a
Olajide <larrm_ola@live.com>
2023-03-14 16:29:47 -0700

    Printing text over a new line

---
+++ 2-printtextoverlines.c
@@ -0,0 +1,20 @@
+//We just showed you how to print text to the screen and in particular how to display multiple lines of text. In this activity, the goal is to print a list of skills you have already mastered to the screen.
+//The list of skills is the following:
+
+//I already know how to:
+//- Print text to the screen.
+//- Start a new line.
+//- Fix errors.
+#include <stdio.h>
+
+/**
+ * main - Print multiple lines.
+ * 
+ * Return: 0
+ */
+ int main(void)
+ {
+     printf("I already know how to:\n- Print text to the screen.\n- Start a new line.\n- Fix Errors.");
+     
+     return (0);
+ }
